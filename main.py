12d13fdc25bb223d9266ff56350b7a4d36a4b5a6
shugaevandrei <66027186+shugaevandrei@users.noreply.github.com>
2022-08-17 22:28:17 +0300

    подключен bootstrap

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return  render_template("blog.html")
+    return  render_template("base.html")
 
 if __name__ == "__main__":
     app.run()
