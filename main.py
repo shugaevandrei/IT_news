baad6463a01793447683ac9a9a253e1c2c8e54a4
shugaevandrei <66027186+shugaevandrei@users.noreply.github.com>
2022-08-17 21:43:17 +0300

    запуск локального сревера с шаблоном

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!!!!!!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return  render_template("blog.html")
+
+if __name__ == "__main__":
+    app.run()
