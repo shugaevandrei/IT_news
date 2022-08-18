8e478a6c54128900c0d8755e401da784eb4fc40b
shugaevandrei <66027186+shugaevandrei@users.noreply.github.com>
2022-08-18 23:56:09 +0300

    добавлена форма для постов

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort #либа для отображения ошибки"404"
 
 def get_db_connection():
@@ -18,6 +19,7 @@ def get_post(post_id):
     return post
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -31,6 +33,23 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                            (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
     
 if __name__ == "__main__":
     app.run()
