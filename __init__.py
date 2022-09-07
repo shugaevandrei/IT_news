c8aa8293188bc9430b668a17414bccb999811b91
shugaevandrei <66027186+shugaevandrei@users.noreply.github.com>
2022-09-07 19:18:45 +0300

    изменена структура проекта

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html', posts=posts)
+    return render_template('/blog/index.html', posts=posts)
 
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -54,7 +54,7 @@ def create():
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
-    return render_template('create.html')
+    return render_template('/blog/create.html')
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
@@ -75,7 +75,7 @@ def edit(id):
             conn.close()
             return redirect(url_for('index'))
 
-    return render_template('edit.html', post=post)
+    return render_template('/blog/edit.html', post=post)
 
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
@@ -85,11 +85,11 @@ def delete(id):
     conn.commit()
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index')) 
+    return redirect(url_for('/blog/index')) 
 
 @app.route('/registration')
 def registration():
-    return render_template('registration.html') 
+    return render_template('/auth/registration.html') 
 
 @app.route('/city')
 def search_city():
@@ -112,6 +112,27 @@ def search_city():
         return f'Current temperature of {city.title()} is {current_temperature_celsius} &#8451;'
     else:
         return f'Error getting temperature for {city.title()}'   
+
+def temperature():
+    API_KEY = '01711b167ecd7d1e5977867e1453a795'  # initialize your key here
+    city = request.args.get('moscow')  # city name passed as argument
+    
+    # call API and convert response into Python dictionary
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
+    response = requests.get(url).json()
+    
+    # error like unknown city name, inavalid api key
+    if response.get('cod') != 200:
+        message = response.get('message', '')
+        return f'Error getting temperature for {city.title()}. Error message = {message}'
+    
+    # get current temperature and convert it into Celsius
+    current_temperature = response.get('main', {}).get('temp')
+    if current_temperature:
+        current_temperature_celsius = round(current_temperature - 273.15, 2)
+        return f'Current temperature of {city.title()} is {current_temperature_celsius} &#8451;'
+    else:
+        return f'Error getting temperature for {city.title()}'          
      
 if __name__ == "__main__":
     app.run()
